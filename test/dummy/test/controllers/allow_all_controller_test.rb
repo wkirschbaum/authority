a838ba44cf7f9d92f031ad9191d86a3008d70c92
wkirschbaum <wkirschbaum@gmail.com>
2018-12-04 16:24:44 +0200

    Update gems

--- test/dummy/test/controllers/allow_all_controller_test.rb
+++ test/dummy/test/controllers/allow_all_controller_test.rb
@@ -4,4 +4,9 @@ class AllowAllControllerTest < ActionDispatch::IntegrationTest
   test 'it throws an exception if allow was not set' do
     assert_raises(Authority::Error) { get '/' }
   end
+
+  test 'it blocks the user if not allowed' do
+    get '/allownotme'
+    assert_response :redirect
+  end
 end
